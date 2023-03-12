dc7bcf89d7de684eb1f09dc24f146d1f51b8a895
A Aziz <aziz2743@hotmail.com>
2023-03-12 22:43:15 +0700

    first commit

---
+++ src/main/java/com/aziz/springboot/dailyspendv01/DailySpendV01Application.java
@@ -0,0 +1,13 @@
+package com.aziz.springboot.dailyspendv01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailySpendV01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailySpendV01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/aziz/springboot/dailyspendv01/DailySpendV01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aziz.springboot.dailyspendv01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailySpendV01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
